ead408d7f3774b9a3df022f2e928c581651dfced
Santosh <sahusantosh19862@gmail.com>
2025-11-27 01:58:19 -0800

    o

--- src/db/connectToMongoDB.ts
+++ src/db/connectToMongoDB.ts
@@ -1,10 +1,9 @@
 import mongoose from 'mongoose';
 mongoose.Promise = Promise;
-const uri: string = "mongodb://127.0.0.1:27017/tshop";
 const mongoDBURL = process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/tshop';
 export async function connectMongoose() {
     try {
-        await mongoose.connect(uri);
+        await mongoose.connect(mongoDBURL);
         console.log("Connected successfully to MongoDB with Mongoose");
 
     } catch (error) {
