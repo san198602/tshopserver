faeac3003ea110ecbb56f81b42103243929c09cc
Santosh <sahusantosh19862@gmail.com>
2025-11-26 22:03:41 -0800

    dasdasd

---
+++ scripts/deploy-on-server.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+# PM2 application name
+PM2_APP_NAME=nodepress
+
+# scripts directory
+SHELL_PATH=$(dirname "$0")
+
+echo "[deploy] starting..."
+
+# Stop the PM2 application
+# pm2 -s –silent: hide all messages
+echo "[deploy] stop pm2 app..."
+pm2 stop "$PM2_APP_NAME" -s
+
+# Upgrade source code & release package
+sh ${SHELL_PATH}/upgrade-on-server.sh
+
+# Restart the PM2 application
+echo "[deploy] restart pm2 app..."
+pm2 restart "$PM2_APP_NAME" -s
+
+echo "[deploy] finished."
--- src/index.ts
+++ src/index.ts
@@ -15,12 +15,15 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json({ limit: '10mb' }));
+app.use(
+  express.urlencoded({ limit: '10mb', extended: true, parameterLimit: 50000 }),
+);
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
 
 app.use(cors({
-    credentials: true
+    credentials: true,
+    optionsSuccessStatus: 200
 }));
 app.use(compression());
 app.use(cookieParser());
